73bc421e90902fcfeacc41f8453779ae8a6dd772
jchekuri2010 <93056377+jchekuri2010@users.noreply.github.com>
2023-06-01 14:55:28 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_block" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name of the key pair"
+  default = "aws-keypair"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances"
+  type        = number
+  default     = 2
+}
+
+variable "ebs_volume_size" {
+  description = "EBS volume size in GB"
+  type        = number
+  default     = 10
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
